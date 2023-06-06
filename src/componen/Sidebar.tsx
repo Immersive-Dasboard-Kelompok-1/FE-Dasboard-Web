c219d5d58d377f6f6ab59bc3074da6bdaff4bcab
Hilal Fadilah <hilalfadilah21@gmail.com>
2023-06-06 08:13:05 +0700

    add sidebar component

---
+++ src/componen/Sidebar.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <section className="min-h-screen flex flex-row bg-gray-100">
+      <div className="flex flex-col w-56 bg-white shadow-2xl overflow-hidden">
+        <div className="flex items-center justify-center h-20 shadow-md">
+          <h1 className="text-3xl uppercase text-indigo-500">Logo</h1>
+        </div>
+        <ul className="flex flex-col py-4">
+          <li>
+            <a
+              href="#"
+              className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
+            >
+              <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+                <i className="bx bx-home"></i>
+              </span>
+              <span className="text-sm font-medium">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
+            >
+              <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+                <i className="bx bx-music"></i>
+              </span>
+              <span className="text-sm font-medium">Mentee</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
+            >
+              <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+                <i className="bx bx-user"></i>
+              </span>
+              <span className="text-sm font-medium">User</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
+            >
+              <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+                <i className="bx bx-drink"></i>
+              </span>
+              <span className="text-sm font-medium">Class</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
